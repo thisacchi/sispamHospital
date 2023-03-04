d5fb73408b15d110de53c77768705716da38218d
Thiago Sacchi <thisacchi@gmail.com>
2023-03-04 08:40:48 -0300

    Correção do Toast com o tailwind

--- components/Toast/Toast.tsx
+++ components/Toast/Toast.tsx
@@ -5,24 +5,41 @@ import style from 'components/Toast/Toast.module.css';
 export default function Toast() {
 
     const { className, cor, mensagem } = useToast();
-    console.log(cor);
+
+    const bg = {
+        'red': 'bg-red-50',
+        'green': 'bg-green-50',
+        'blue': 'bg-blue-50'
+    };
+
+    const border = {
+        'red': 'border-l-red-300',
+        'green': 'border-l-green-300',
+        'blue': 'border-l-blue-300'
+    };
+
+    const colorText = {
+        'red': 'text-red-600',
+        'green': 'text-green-600',
+        'blue': 'text-blue-600'
+    }
 
     return (
 
         <div className={`flex justify-center z-50 absolute w-screen ${style[className]}`}>
             <div className={`w-1/3 relative h-28 
-                bg-${cor}-50 bg-red-50 rounded-md border-l-2 
-                    border-l-${cor}-300 mt-2 `}>
+                ${bg[cor]} bg-red-50 rounded-md border-l-2 
+                    ${border[cor]} mt-2 `}>
                 <div className="flex flex-col">
                     <div className="flex justify-end pr-2">
                         <button>X</button>
                     </div>
                     <div className="text-center">
-                        <h3 className={`text-${cor}-600 font-bold`}>{mensagem?.titulo}</h3>
+                        <h3 className={`${colorText[cor]} font-bold`}>{mensagem?.titulo}</h3>
                     </div>
                     <hr />
                     <div className="mt-4 text-center">
-                        <p className={`text-${cor}-600`}>
+                        <p className={`${colorText[cor]}`}>
                             {mensagem?.mensagem}
                         </p>
                     </div>
